4b5f894af67d167e8a9d7d2fe1556ad90f7803b2
Вадим Матырин <maturin030700@gmail.com>
2019-10-21 21:31:57 +0300

    Add WeekDays enum with flags. TeacherInfo entity - Delete SubjectId from teacher, change MinimumWage type to double, add PreferredDaysOfWeek field

--- Tuto.WebAPI/Tuto.Domain/Models/TeacherInfo.cs
+++ Tuto.WebAPI/Tuto.Domain/Models/TeacherInfo.cs
@@ -5,13 +5,24 @@ using Tuto.Domain.Models.Interfaces;
 
 namespace Tuto.Domain.Models
 {
+    [Flags]
+    public enum WeekDays
+    {
+        Monday = 2,
+        Tuesday = 4,
+        Wednesday = 8,
+        Thursday = 16,
+        Friday = 32,
+        Sunday = 64,
+        Saturday = 128,
+    }
     public class TeacherInfo : IODataEntity
     {
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
-        public Guid SubjectId { get; set; }
-        [Range(0, Int32.MaxValue)]
-        public int MinimumWage { get; set; }
+        [Range(0, double.MaxValue)]
+        public double MinimumWage { get; set; }
+        public WeekDays? PreferredDaysOfWeek { get;set; }
         public User User { get; set; }
         public virtual ICollection<TeacherSubject> TeacherSubjects { get; set; }
     }
