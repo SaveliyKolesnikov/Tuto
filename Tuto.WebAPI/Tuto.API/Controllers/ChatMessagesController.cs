b9ab5397ee23b4295f9b24084c857dac531777fb
Savelii <s.koles33@gmail.com>
2019-11-30 22:13:24 +0200

    Features/chat (#11)
    * stage
    
    * Intergate chat hub into chat messages odata controller
    
    * Add first message validation

--- Tuto.WebAPI/Tuto.API/Controllers/ChatMessagesController.cs
+++ Tuto.WebAPI/Tuto.API/Controllers/ChatMessagesController.cs
@@ -1,12 +1,97 @@
-﻿using Tuto.Domain.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using Tuto.API.Authorization;
+using Tuto.API.Hubs;
+using Tuto.Domain.Models;
 using Tuto.Domain.Repositories;
+using Tuto.Services.Interfaces;
 
 namespace Tuto.API.Controllers
 {
+    [AuthFilter]
     public class ChatMessagesController : ODataControllerBase<ChatMessage>
     {
-        public ChatMessagesController(IRepository<ChatMessage> entityRepository) : base(entityRepository)
+        private readonly IHubContext<ChatHub> _chatHub;
+        private readonly IRepository<User> _userRepository;
+        private readonly IAppUserManager _userManager;
+
+        public ChatMessagesController(IRepository<ChatMessage> entityRepository,  
+            IHubContext<ChatHub> chatHub, IRepository<User> userRepository, IAppUserManager userManager) : base(entityRepository)
         {
+            _chatHub = chatHub;
+            _userRepository = userRepository;
+            _userManager = userManager;
+        }
+
+        public override async Task<IActionResult> Post(ChatMessage entity)
+        {
+            var (isValid, errorMessage) = await IsMessageValid(entity);
+            if (!isValid) return BadRequest(errorMessage);
+
+            var actionResult = await base.Post(entity);
+            try
+            {
+                await SendMessageToHubAsync(entity);
+            }
+            catch (AuthenticationException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e);
+            }
+            return actionResult;
+        }
+
+        private async Task SendMessageToHubAsync(ChatMessage message)
+        {
+            var recipientConnection = ChatHub.Connections.FirstOrDefault(c => c.Value == message.RecipientId);
+            if (!recipientConnection.Equals(default(KeyValuePair<string, Guid>)))
+            {
+                await _chatHub.Clients.Client(recipientConnection.Key).SendAsync("receiveMessage", message);
+            }
+            var senderConnection = ChatHub.Connections.FirstOrDefault(c => c.Value == message.SenderId);
+            if (!senderConnection.Equals(default(KeyValuePair<string, Guid>)))
+            {
+                await _chatHub.Clients.Client(senderConnection.Key).SendAsync("receiveMessage", message);
+            }
+
+        }
+
+        private async Task<(bool isValid, string errorMessage)> IsMessageValid(ChatMessage message)
+        {
+            _userManager.TryGetUserId(User, out var appUserId);
+            var sender = await _userRepository.Read().Include(u => u.Roles).FirstOrDefaultAsync(u => u.Id == appUserId);
+            if (sender.Roles.Any(r => r.Name == AuthRoles.Teacher) && !await _entityRepository.Read()
+                    .AnyAsync(m => m.SenderId == message.SenderId && m.RecipientId == message.RecipientId))
+            {
+                return (false, "Teacher cannot send first message.");
+            } 
+
+            if (message.SenderId != sender.Id)
+                return (false, "Fake sender id.");
+
+            if (!await _userRepository.Read().AnyAsync(u => u.Id == message.RecipientId))
+                return (false, $"User with id {message.RecipientId} doesn't exist.");
+
+            message.Text = new Regex(@"&nbsp;?").Replace(message.Text, " ");
+            message.Text = HttpUtility.HtmlDecode(message.Text);
+            if (string.IsNullOrWhiteSpace(message.Text))
+                return (false, "Message cannot be empty.");
+
+            if (message.Text.Length > 4096)
+                return (false, "Message text length must be less then 4096.");
+
+            return (true, string.Empty);
         }
     }
 }
---
+++ Tuto.WebAPI/Tuto.API/Hubs/ChatHub.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Tuto.Domain;
+using Tuto.Services.Interfaces;
+
+namespace Tuto.API.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly IAppUserManager _userManager;
+        private readonly ApplicationDbContext _applicationDbContext;
+        public static readonly ConcurrentDictionary<string, Guid> Connections = new ConcurrentDictionary<string, Guid>();
+
+        public ChatHub(IAppUserManager userManager,
+            ApplicationDbContext applicationDbContext)
+        {
+            _userManager = userManager;
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            if (_userManager.TryGetUserId(Context.User, out var appUserId))
+            {
+                Connections.TryAdd(Context.ConnectionId, appUserId);
+            }
+
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            Connections.TryRemove(Context.ConnectionId, out _);
+
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
