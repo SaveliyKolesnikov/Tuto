91beb61bdb81e4a5f5a4eada05ebce0fa893bc76
Вадим Матырин <maturin030700@gmail.com>
2019-12-01 14:31:27 +0200

    Add subject id field to TeacherInfo entity

---
+++ Tuto.WebAPI/Tuto.Domain/Migrations/20191201122803_Add-subjectId-to-teacheSubject.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tuto.Domain.Migrations
+{
+    public partial class AddsubjectIdtoteacheSubject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "SubjectId",
+                table: "TeacherSubjects",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeacherSubjects_SubjectId",
+                table: "TeacherSubjects",
+                column: "SubjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TeacherSubjects_Subjects_SubjectId",
+                table: "TeacherSubjects",
+                column: "SubjectId",
+                principalTable: "Subjects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TeacherSubjects_Subjects_SubjectId",
+                table: "TeacherSubjects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TeacherSubjects_SubjectId",
+                table: "TeacherSubjects");
+
+            migrationBuilder.DropColumn(
+                name: "SubjectId",
+                table: "TeacherSubjects");
+        }
+    }
+}
